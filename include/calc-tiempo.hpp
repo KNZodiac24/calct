f1e0f64d365323b6adca42be0a71ac3a3491574e
KNZodiac24 <navakenny_16@hotmail.com>
2026-04-12 17:23:30 -0500

    Reestructuración de proyecto

--- include/calc-tiempo.hpp
+++ include/calc-tiempo.hpp
@@ -1,13 +1,12 @@
 #ifndef CALC_TIEMPO_HPP
 #define CALC_TIEMPO_HPP
 
-#include <cstdlib>
 #include <iostream>
 #include <cmath>
 #include <vector>
 #include <string>
 #include <expected>
-#include "ctre.hpp"
+#include <ctre.hpp>
 
 #define HORAS_EN_SEGS 3600
 #define MINS_EN_SEGS  60
@@ -29,7 +28,7 @@ inline void imprimirMensajeAyuda(){
     std::cout << "-------------------- CALCULADORA DE TIEMPO --------------------\n---------------------------------------------------------------\n\n"
               << "Se deben ingresar los tiempos con el siguiente formato: hh:mm:ss\nEl nivel de detalle máximo aceptado es en horas, y el mínimo en segundos. \n---------------------------------------------------------------\n"
               << "Ejemplos de formato (formato -> equivalencia en lenguaje natural):\n10:31:27 -> 10 horas, 31 minutos y 27 segundos\n01:03:07 -> 1 hora, 3 minutos y 7 segundos\n2:1:0 -> 2 horas, 1 minuto y 0 segundos\n10:3 -> 10 minutos y 3 segundos\n5:07 -> 5 minutos y 7 segundos\n34 -> 34 segundos\n03 -> 3 segundos\n9 -> 9 segundos\n\n"
-              << "Ejemplos de uso:\nSuma de tiempos:\n\t23:19 + 56 -> 24:15\n\t1:3:4 + 2:34:1 -> 3:37:5\nResta de tiempos:\n\t4:56:12 - 1:23:09 -> 3:33:3\n\t57:29 - 19:51 -> 37:38";
+              << "Ejemplos de uso:\nSuma de tiempos:\n\t23:19 + 56 -> 24:15\n\t1:3:4 + 2:34:1 -> 3:37:5\nResta de tiempos:\n\t4:56:12 - 1:23:09 -> 3:33:3\n\t57:29 - 19:51 -> 37:38\n";
 }
 
 /**
